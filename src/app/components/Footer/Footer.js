fabf47b23dac535d5a6df957953c5e6b368f59c7
tduncan14 <nbatre1@yahoo.com>
2026-01-13 11:24:36 -0600

    keep moving forward

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -1,10 +1,63 @@
+"use client"
+
 import React from 'react'
+import Link from 'next/link'
 import './Footer.css'
 
 const Footer = () => {
     return (
         <div className="Footer">
-            Footer
+            <div className="leftFootWrapper">
+                <div className="leftFooter">
+                    <div className="leftLeft">
+                        <div className="Logo LogoFooter">
+                            <div className="triangle-logo"></div>
+                            <h1>PHOTOSNAP</h1>
+                        </div>
+
+
+                        <div className="socialIcons">
+
+                            <a href="www.facebook.com"><img src="/assets/shared/desktop/facebook.svg" /></a>
+                            <a href="www.facebook.com"><img src="/assets/shared/desktop/youtube.svg" /></a>
+                            <a href="www.facebook.com"><img src="/assets/shared/desktop/twitter.svg" /></a>
+                            <a href="www.facebook.com"><img src="/assets/shared/desktop/pinterest.svg" /></a>
+                            <a href="www.facebook.com"><img src="/assets/shared/desktop/instagram.svg" /></a>
+
+                        </div>
+
+                    </div>
+
+                    <div className="leftRight">
+                        <ul>
+                            <li>HOME</li>
+                            <li>STORIES</li>
+                            <li>FEATURES</li>
+                            <li>PRICING</li>
+                        </ul>
+
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div className="rightFooter">
+
+                <div className="arrowMEe">
+                    <h3>GET AN INVITE</h3>
+
+                    <img src="/assets/shared/desktop/arrow.svg" />
+                </div>
+
+
+
+                <div className="copyright">
+                    <h3>Copyright 2019. All Rights Reserved</h3>
+                </div>
+
+
+            </div>
         </div>
     )
 }
--- src/app/components/GalleryStories/Stories.js
+++ src/app/components/GalleryStories/Stories.js
@@ -22,7 +22,8 @@ const Stories = ({ Title, text, bg }) => {
                 <div className="readStory">
                     <h3>READ STORY</h3>
 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-right"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M5 12l14 0" /><path d="M15 16l4 -4" /><path d="M15 8l4 4" /></svg>
+                    <img className='svgArrow' src="./assets/shared/desktop/arrow.svg" />
+
                 </div>
 
 

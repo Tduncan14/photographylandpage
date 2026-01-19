906a8ca0860baf27b99cc98f3c2b5588fbd8a445
tduncan14 <nbatre1@yahoo.com>
2026-01-19 17:55:09 -0600

    keep moving forward

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -1,37 +1,48 @@
+"use client"
+
 import React from 'react'
 import './Header.css'
 import Link from 'next/link'
-
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+
   return (
     <div className="header">
 
       <div className="Logo">
-        <Link className="Logo" href="/">
+        <Link href="/">
           <div className="triangle-logo"></div>
           <h1>PHOTOSNAP</h1>
         </Link>
       </div>
 
-
       <nav className="nav">
+        <Link
+          href="/stories"
+          className={`nav-link ${pathname === "/stories" ? "active" : ""}`}
+        >
+          Stories
+        </Link>
 
-        <Link href="/stories">Stories</Link>
+        <Link
+          href="/features"
+          className={`nav-link ${pathname === "/features" ? "active" : ""}`}
+        >
+          Feature
+        </Link>
 
-
-        <Link href="/features">Feature</Link>
-
-        <Link href="/pricing"> Pricing </Link>
-
-
+        <Link
+          href="/pricing"
+          className={`nav-link ${pathname === "/pricing" ? "active" : ""}`}
+        >
+          Pricing
+        </Link>
       </nav>
 
-
       <div className="button">
-
-        <button className="inviteButton"> GET AN INVITE </button>
-
+        <button className="inviteButton">GET AN INVITE</button>
       </div>
 
     </div>
